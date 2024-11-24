5ab2ea74cd740aaed3aaf6a46f623b781e4a998f
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2024-11-24 21:04:57 +0100

    no images

--- src/src/index.js
+++ src/src/index.js
@@ -1,11 +1,18 @@
 // src/templates/populated-worker/src/index.js
-import html from "./static/preGame/teams.html"
+import html from "./static/pre_game/teams.html"
+
 
 var src_default = {
   async fetch(request, env) {
     /*const { DATABASE } = env;
     const stmt = DATABASE.prepare("SELECT * FROM comments LIMIT 3");
     const { results } = await stmt.all();*/
+
+    let url = String(request.url);
+
+    if (url.startsWith("https://philosophie.tcku.dev/static")) {
+      return staticHandler(request);
+    }
 
     // Standard-Route: HTML-Seite rendern
     return new Response(html, {
