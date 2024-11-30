276d2dfd194a75ac3b7849934e170faeb662598e
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2024-11-30 01:02:01 +0100

    start Backend

--- src/DbManager.py
+++ src/DbManager.py
@@ -1,5 +1,19 @@
 import json
 
+
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip('#')
+
+    if len(hex_color) != 6:
+        raise ValueError("Ungültiger Hex-Farbwert. Erwarte Format #RRGGBB")
+
+    r = int(hex_color[0:2], 16)
+    g = int(hex_color[2:4], 16)
+    b = int(hex_color[4:6], 16)
+
+    return (r, g, b)
+
+
 class DB:
     def __init__(self, path: str):
         self._db_path: str = path
@@ -21,3 +35,69 @@ class DB:
         with open(self._db_path, "r") as js:
             db: dict = json.load(js)
         return db["user"][ip]
+
+    def getUsers(self) -> dict:
+        db: dict = dict()
+        with open(self._db_path, "r") as js:
+            db: dict = json.load(js)
+        return db["user"]
+
+    def assignTeam(self, user: str, team: int) -> None:
+        db: dict = dict()
+        with open(self._db_path, "r") as js:
+            db: dict = json.load(js)
+        oldTeam: str = str(db["user"][user]["team"])
+        db["teams"][oldTeam]["user_count"] -= 1
+        if db["teams"][oldTeam]["user_count"] <= 0:
+            del db["teams"][oldTeam]
+        db["user"][user]["team"] = team
+        db["teams"][str(team)]["user_count"] += 1
+
+        with open(self._db_path, "w") as js:
+            json.dump(db, js, indent=4)
+
+    def getTeams(self) -> list:
+        db: dict = dict()
+        with open(self._db_path, "r") as js:
+            db: dict = json.load(js)
+        teams: list = list(db["teams"].values())
+        for team in teams:
+            team["research_field"] = db["research_field"][team["research_field"]]
+        return teams
+
+    def createTeam(self, name: str, color: str,
+                   research_field: str, creator: str) -> int:
+        db: dict = dict()
+        with open(self._db_path, "r") as js:
+            db: dict = json.load(js)
+        fields: list = db["research_field"]
+        for i in range(len(fields)):
+            if fields[i] == research_field:
+                break
+        creator: str = db["user"][creator]["name"]
+        team: dict = {
+            "name": name,
+            "color": hex_to_rgb(color),
+            "research_field": i,
+            "creator": creator,
+            "country": "",
+            "user_count": 0,
+            "chosen_country": {}
+        }
+        largest: int = -1
+        for key in db["teams"].keys():
+            largest = max(int(key), largest)
+
+        id: int = largest+1
+        db["teams"][str(id)] = team
+
+        with open(self._db_path, "w") as js:
+            json.dump(db, js, indent=4)
+
+        return id
+
+    def hasGameStarted(self) -> bool:
+        db: dict = dict()
+        with open(self._db_path, "r") as js:
+            db: dict = json.load(js)
+        return db["game"]["state"]["started"]
--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 import http.server
+import urllib.parse
 from socketserver import ThreadingMixIn
 import logging
 from PathManager import PathsManager
 from DbManager import DB
 import json
+import hashlib
 
+ADDR: str = "127.0.0.1"
 
 def file_to_bit(file_path):
     with open(file_path, 'rb') as f:  # Open file in binary mode
         byte_data = f.read()  # Read the entire file content in bytes
         # Convert each byte to a bit string and join them together
     return byte_data
+
 
 class ThreadedHTTPServer(ThreadingMixIn, http.server.HTTPServer):
     """Handle requests in a separate thread."""
@@ -46,13 +50,29 @@ class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         super().__init__(request, client_address, server)
 
     def do_GET(self):
+        Base: DB = DB(PathsManager().getPath("db"))
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(file_to_bit(PathsManager().getPath("static", "pre_game", "creatUser")))
             return
-
+        if self.path == "/teams" and not Base.hasGameStarted():
+            Users: set = set(Base.getUsers().keys())
+            if self.client_address[0] in Users:
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(file_to_bit(PathsManager().getPath("static", "pre_game", "teams")))
+            else:
+                self.send_response(302)
+                self.send_header('Location', f'http://{ADDR}')
+                self.end_headers()
+        if self.path == "/landingPage" and not Base.hasGameStarted():
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(file_to_bit(PathsManager().getPath("static", "pre_game", "landingPage")))
         if self.path.startswith("/static"):
             path: list = self.path.split("/")
             path: list = [sub for sub in path if sub != ""]
@@ -65,7 +85,10 @@ class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
             elif path[1] == "img":
                 img = file_to_bit(PathsManager().getPath(*path))
                 self.send_response(200)
-                self.send_header("Content-type", "image/jpg")
+                if path[-1][:-3] == "jpg":
+                    self.send_header("Content-type", "image/jpg")
+                elif path[-1][:-3] == "png":
+                    self.send_header("Content-type", "image/png")
                 self.send_header("Content-Length", len(img))
                 self.end_headers()
                 self.wfile.write(img)
@@ -77,9 +100,6 @@ class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(font)
 
-
-
-
     def do_POST(self):
         path: list = self.path.split("/")
         path: list = [sub for sub in path if sub != ""]
@@ -87,12 +107,20 @@ class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
             content_length = int(self.headers.get('Content-Length', 0))
             post_data = self.rfile.read(content_length)
             post_data = str(post_data)
-            username: str = post_data[post_data.find("=")+1:-1]
-            DB(PathsManager().getPath("db")).createUser(username, self.client_address[0])
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(file_to_bit(PathsManager().getPath("static", "pre_game", "teams")))
+            username: str = post_data[post_data.find("=") + 1:-1]
+            username_hash = hashlib.sha256(username.encode())
+            username_hash = username_hash.hexdigest()
+            if username_hash == "5823d1bc9416c2996bc9572da20cc9efa617fc036181e2d42c7e8b198519e7fa":
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(file_to_bit(PathsManager().getPath("static", "admin", "lander")))
+            elif not DB(PathsManager().getPath("db")).hasGameStarted():
+                DB(PathsManager().getPath("db")).createUser(username, self.client_address[0])
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(file_to_bit(PathsManager().getPath("static", "pre_game", "teams")))
 
         elif path[0] == "api":
             content_length = int(self.headers.get('Content-Length', 0))
@@ -106,11 +134,31 @@ class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
                 self.send_header("Content-type", "application/json")
                 self.end_headers()
                 self.wfile.write(response_json.encode("utf-8"))
+            elif post_data["message"] == "teams":
+                teams: list = DB(PathsManager().getPath("db")).getTeams()
+                data: dict = {"teams": teams}
+                response_json = json.dumps(data)
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                self.wfile.write(response_json.encode("utf-8"))
+        elif path[0] == "team_create":
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
 
+            parsed_data = urllib.parse.parse_qs(post_data.decode('utf-8'))
+            form_data = {key: values[0] for key, values in parsed_data.items()}
+            Base: DB = DB(PathsManager().getPath("db"))
+            teamId: int = Base.createTeam(form_data["Teamname"], form_data["Teamfarbe"],
+                                          form_data["gebiet"], self.client_address[0])
+            Base.assignTeam(self.client_address[0], teamId)
+            self.send_response(302)
+            self.send_header('Location', f'http://{ADDR}/teams')
+            self.end_headers()
 
 
 def run_server():
-    server_address = ('', 80)
+    server_address = (ADDR, 80)
     httpd = ThreadedHTTPServer(server_address, SimpleHTTPRequestHandler)
 
     # SSL-Kontext konfigurieren
@@ -120,7 +168,7 @@ def run_server():
     # SSL auf den Server-Socket anwenden
     # httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
-    print("HTTP-Server läuft auf http://localhost:80")
+    print(f"HTTP-Server läuft auf http://{ADDR}:80")
     httpd.serve_forever()
 
 
